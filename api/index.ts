52d3b608d7821c76b230883dc1e95aa00a68d339
devjiwonchoi <devjiwonchoi@gmail.com>
2023-10-13 03:22:33 +0900

    refac: remove unncessary color map

--- api/index.ts
+++ api/index.ts
@@ -61,8 +61,13 @@ app.get('/api', async (req, res) => {
     const limit = req.query['languages.limit'] as string
     const ignored = req.query['languages.ignored'] as string
 
-    const languages = await fetchLanguagesData({ variables, limit, ignored })
-    result = { ...result, ...languages }
+    const languagesData = await fetchLanguagesData({
+      variables,
+      limit,
+      ignored,
+    })
+
+    result = { ...result, languages: languagesData }
   }
 
   res.json(result)
--- src/features/languages/index.ts
+++ src/features/languages/index.ts
@@ -2,7 +2,6 @@ import { fetcher, graphqlParser } from '../../utils'
 
 function resolveLanguagesNodes(nodes: any[], limit: number, ignored?: string) {
   const languagesMap = new Map<string, any>()
-  const languagesColorsMap = new Map<string, any>()
 
   nodes.forEach((node: any) => {
     const {
@@ -15,19 +14,18 @@ function resolveLanguagesNodes(nodes: any[], limit: number, ignored?: string) {
 
       if (languagesMap.has(language)) {
         const currentSize = languagesMap.get(language).size
-        languagesMap.set(language, { language, size: currentSize + size })
+        languagesMap.set(language, {
+          language,
+          size: currentSize + size,
+          color,
+        })
       } else {
-        languagesMap.set(language, { language, size })
-      }
-
-      if (!languagesColorsMap.has(language)) {
-        languagesColorsMap.set(language, { language, color })
+        languagesMap.set(language, { language, size, color })
       }
     })
   })
 
   let languages = Array.from(languagesMap.values())
-  let languageColors = Array.from(languagesColorsMap.values())
 
   if (ignored) {
     const excludesArray = ignored
@@ -37,17 +35,13 @@ function resolveLanguagesNodes(nodes: any[], limit: number, ignored?: string) {
     languages = languages.filter(
       ({ language }) => !excludesArray.includes(language.toLowerCase()),
     )
-
-    languageColors = languageColors.filter(
-      ({ language }) => !excludesArray.includes(language.toLowerCase()),
-    )
   }
 
   languages.sort((a, b) => b.size - a.size)
   // TODO: find a way to query with limit
   languages = languages.slice(0, limit)
 
-  return { languages, languageColors }
+  return languages
 }
 
 export async function fetchLanguagesData({
