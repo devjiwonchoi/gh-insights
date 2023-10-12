ca65089f771774726b45f6026f4d70cfe5b9bdf4
devjiwonchoi <devjiwonchoi@gmail.com>
2023-10-13 00:41:07 +0900

    chore: modify invalid-route test

--- test/invalid-routes.test.ts
+++ test/invalid-routes.test.ts
@@ -5,6 +5,8 @@ describe('Missing username query', () => {
   it('should return 400 status with info message', async () => {
     const response = await request(app).get('/api')
     expect(response.status).toBe(400)
-    expect(response.text).toBe('Please provide a valid username')
+    expect(response.body.message).toBe(
+      'Bad Request. Please provide a valid username.',
+    )
   })
 })
