c2f2209255398f69ce44bc7c063a087f97531967
devjiwonchoi <devjiwonchoi@gmail.com>
2023-10-12 14:10:32 +0900

    chore: add contribs

--- src/utils.ts
+++ src/utils.ts
@@ -95,3 +95,52 @@ export function handleLanguages(
 
   return { langs, langColors }
 }
+
+export function handleContributions(
+  nodes: any[],
+  stars?: string,
+  accountType?: string,
+  ignored?: string,
+) {
+  const contribsMap = new Map<string, string>()
+
+  nodes.forEach((node: any) => {
+    const {
+      stargazerCount,
+      isInOrganization,
+      name,
+      owner: { login, avatarUrl },
+    } = node
+
+    if (stars) {
+      const starsNumber = Number(stars)
+      if (stargazerCount < starsNumber) return
+    }
+
+    // if accountType is 'all', pass all repos. else, pass only isInOrganization repos
+    if (accountType !== 'all' && !isInOrganization) return
+
+    if (ignored) {
+      console.log(name)
+      const ignoredArray = ignored.split(',').map((name) => name.toLowerCase())
+      if (
+        ignoredArray.includes(name.toLowerCase()) ||
+        ignoredArray.includes(login.toLowerCase())
+      )
+        return
+    }
+
+    if (node.nameWithOwner) {
+      contribsMap.set(node.nameWithOwner, avatarUrl)
+    } else {
+      contribsMap.set(name, avatarUrl)
+    }
+  })
+
+  const contribs = Array.from(contribsMap.entries()).map(([name, avatar]) => ({
+    name,
+    avatar,
+  }))
+
+  return { contribs }
+}
