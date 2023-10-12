c233e98607e9cf10e5b015d64905f1c388862ba3
devjiwonchoi <devjiwonchoi@gmail.com>
2023-10-12 21:12:02 +0900

    refac: contribs

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import handleDiscussions from '../src/features/discussions'
 import handleLanguages from '../src/features/languages'
-import { fetcher, handleContributions } from '../src/utils'
+import handleContributions from '../src/features/contributions'
 
 const app = express()
 
@@ -9,12 +9,9 @@ app.get('/api', async (req, res) => {
   const { username, contributions, discussions, languages } = req.query
   if (!username) return res.status(400).send('Please provide a valid username')
 
-  // TODO: refac as const, add as variables.x = y
-  let variables: Record<string, any> = { login: username as string }
   let result = {}
 
   if (contributions) {
-    const query: string = require('../src/queries/contributions').default
     const stars = req.query['contributions.stars'] as string
     const owner = req.query['contributions.owner'] as string
     // Allowed types: org, all
@@ -24,57 +21,15 @@ app.get('/api', async (req, res) => {
     // Queries: commit, issue, pull, repo, review
     const type = req.query['contributions.type'] as string
 
-    if (type) {
-      const contributionTypes = type.split(',').map((el) => {
-        if (el === 'pull') return 'PULL_REQUEST'
-        if (el === 'repo') return 'REPOSITORY'
-        if (el === 'review') return 'PULL_REQUEST_REVIEW'
-
-        return el.toUpperCase()
-      })
-
-      variables = { ...variables, contributionTypes }
-    } else {
-      const contributionTypes = [
-        'COMMIT',
-        'ISSUE',
-        'PULL_REQUEST',
-        'REPOSITORY',
-        'PULL_REQUEST_REVIEW',
-      ]
-
-      variables.contributionTypes = contributionTypes
-    }
-
-    if (owner) variables = { ...variables, owner: true }
-
-    let nodesArray: any[] = []
-    // init loop
-    let hasNextPage = true
-
-    while (hasNextPage) {
-      const {
-        data: {
-          user: {
-            repositoriesContributedTo: {
-              nodes,
-              pageInfo: { hasNextPage: newHasNextPage, endCursor },
-            },
-          },
-        },
-      } = await fetcher(query, variables)
-      nodesArray = [...nodesArray, ...nodes]
-      variables = { ...variables, cursor: endCursor }
-      hasNextPage = newHasNextPage
-
-      const contribs = handleContributions(
-        nodesArray,
-        stars,
-        accountType,
-        ignored,
-      )
-      result = { ...result, ...contribs }
-    }
+    const contribs = await handleContributions({
+      login: username as string,
+      stars,
+      owner,
+      accountType,
+      ignored,
+      type,
+    })
+    result = { ...result, contribs }
   }
 
   if (discussions) {
---
+++ src/features/contributions/index.ts
@@ -0,0 +1,119 @@
+import query from '../../queries/contributions'
+import { fetcher } from '../../utils'
+
+function handleNodes(
+  nodes: any[],
+  stars?: string,
+  accountType?: string,
+  ignored?: string,
+) {
+  const contribsMap = new Map<string, string>()
+
+  nodes.forEach((node: any) => {
+    const {
+      stargazerCount,
+      isInOrganization,
+      name,
+      owner: { login, avatarUrl },
+    } = node
+
+    if (stars) {
+      const starsNumber = Number(stars)
+      if (stargazerCount < starsNumber) return
+    }
+
+    // if accountType is 'all', pass all repos. else, pass only isInOrganization repos
+    if (accountType !== 'all' && !isInOrganization) return
+
+    if (ignored) {
+      const ignoredArray = ignored.split(',').map((name) => name.toLowerCase())
+      if (
+        ignoredArray.includes(name.toLowerCase()) ||
+        ignoredArray.includes(login.toLowerCase())
+      )
+        return
+    }
+
+    if (node.nameWithOwner) {
+      contribsMap.set(node.nameWithOwner, avatarUrl)
+    } else {
+      contribsMap.set(name, avatarUrl)
+    }
+  })
+
+  const contribs = Array.from(contribsMap.entries()).map(
+    ([name, avatarUrl]) => ({
+      name,
+      avatarUrl,
+    }),
+  )
+
+  return contribs
+}
+
+async function getUserContributions(variables: any) {
+  let nodesArray: any[] = []
+  // init loop
+  let hasNextPage = true
+
+  while (hasNextPage) {
+    const {
+      data: {
+        user: {
+          repositoriesContributedTo: {
+            nodes,
+            pageInfo: { hasNextPage: newHasNextPage, endCursor },
+          },
+        },
+      },
+    } = await fetcher(query, variables)
+    nodesArray = [...nodesArray, ...nodes]
+    variables = { ...variables, cursor: endCursor }
+    hasNextPage = newHasNextPage
+  }
+
+  return nodesArray
+}
+
+export default async function resolveRequestQueries({
+  login,
+  stars,
+  owner,
+  accountType,
+  ignored,
+  type,
+}: {
+  login: string
+  stars: string
+  owner: string
+  accountType: string
+  ignored: string
+  type: string
+}) {
+  let variables: Record<string, any> = { login }
+  let contributionTypes: string[] = []
+  if (type) {
+    contributionTypes = type.split(',').map((el) => {
+      if (el === 'pull') return 'PULL_REQUEST'
+      if (el === 'repo') return 'REPOSITORY'
+      if (el === 'review') return 'PULL_REQUEST_REVIEW'
+
+      return el.toUpperCase()
+    })
+  } else {
+    contributionTypes = [
+      'COMMIT',
+      'ISSUE',
+      'PULL_REQUEST',
+      'REPOSITORY',
+      'PULL_REQUEST_REVIEW',
+    ]
+  }
+  variables.contributionTypes = contributionTypes
+
+  if (owner) variables = { ...variables, owner: true }
+
+  const nodesArray = await getUserContributions(variables)
+
+  return handleNodes(nodesArray, stars, accountType, ignored)
+}
